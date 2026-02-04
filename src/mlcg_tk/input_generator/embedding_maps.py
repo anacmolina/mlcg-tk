fdc7a762cb89b9fe59b72c5fa111e336e0dfbc49
Ana Molina <anac.molina@udea.edu.co>
2026-02-04 15:25:32 +0100

    new embedding for the WRC protein

--- src/mlcg_tk/input_generator/embedding_maps.py
+++ src/mlcg_tk/input_generator/embedding_maps.py
@@ -121,3 +121,60 @@ def embedding_ca(atom_df):
         print(f"Unknown atom name given: {name}")
         atom_type = "NA"
     return atom_type
+
+
+class CGEmbeddingMapCA_WRC(CGEmbeddingMap):
+    """
+    One-bead embedding map defined by:
+        - CA : backbone alpha carbon, carrying aminoacid identity
+        - PROa : Adding an extended embedding for the PROLINE in two specific bonds 
+    """
+
+    ext_embedding = {
+        "PROa": 21,
+    }
+
+    def __init__(self):
+        ca_dict = {key: emb for key, emb in embedding_map_fivebead.items() if emb <= 20}
+        ca_dict.update(self.ext_embedding)
+        super().__init__(ca_dict)
+
+
+
+def embedding_ca_WRC(atom_df):
+    """
+    Helper function for mapping high-resolution topology to
+    CA WRC modified embedding map.
+    """
+
+    inv_CGEmbeddingMapCA_WRC = {label: resi for resi, label in CGEmbeddingMapCA_WRC().items()}
+
+    ext_embedding = {
+       1349 : 21,
+       2422 : 21
+    }
+
+    name, res, idxSeq, idx = atom_df["name"], atom_df["resName"], atom_df["resSeq"], atom_df['serial']
+
+    if name == "CA":
+
+        if idxSeq in ext_embedding.keys():
+            
+            new_res = inv_CGEmbeddingMapCA_WRC[ext_embedding[idxSeq]]
+
+            if res in new_res:
+                atom_type = CGEmbeddingMapCA_WRC()[new_res]
+                print(f"Residue {res} with residue idx {idxSeq} and atom idx {idx} was mapped to {new_res} in WRC embedding with atom type {atom_type}.")
+            else:
+                raise ValueError(
+                    f"Residue  {res} has no special mapping."
+                )
+
+        else:
+            atom_type = embedding_map_fivebead[res]
+
+    else:
+        print(f"Unknown atom name given: {name}")
+        atom_type = "NA"
+    
+    return atom_type
